cde1dd3219e84f7a6a8775c691bb94a26ea7a08f
Tisham Dhar <whatnickd@gmail.com>
2021-03-01 23:15:10 +1100

    Add osgeo4w dependency tree
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>

---
+++ site/deps_osgeo4w.js
@@ -0,0 +1,694 @@
+var pyData = [
+    {
+        "package": {
+            "key": "urllib3",
+            "package_name": "urllib3",
+            "installed_version": "1.25.9"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "typing-extensions",
+            "package_name": "typing-extensions",
+            "installed_version": "3.7.4.3"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "typed-ast",
+            "package_name": "typed-ast",
+            "installed_version": "1.4.1"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "traitlets",
+            "package_name": "traitlets",
+            "installed_version": "4.3.3"
+        },
+        "dependencies": [
+            {
+                "key": "ipython-genutils",
+                "package_name": "ipython-genutils",
+                "installed_version": "0.2.0",
+                "required_version": null
+            },
+            {
+                "key": "six",
+                "package_name": "six",
+                "installed_version": "1.15.0",
+                "required_version": null
+            },
+            {
+                "key": "decorator",
+                "package_name": "decorator",
+                "installed_version": "4.4.1",
+                "required_version": null
+            }
+        ]
+    },
+    {
+        "package": {
+            "key": "toml",
+            "package_name": "toml",
+            "installed_version": "0.10.1"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "stdlib-list",
+            "package_name": "stdlib-list",
+            "installed_version": "0.8.0"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "six",
+            "package_name": "six",
+            "installed_version": "1.15.0"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "sip",
+            "package_name": "sip",
+            "installed_version": "4.19.14"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "setuptools",
+            "package_name": "setuptools",
+            "installed_version": "50.3.0"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "retrying",
+            "package_name": "retrying",
+            "installed_version": "1.3.3"
+        },
+        "dependencies": [
+            {
+                "key": "six",
+                "package_name": "six",
+                "installed_version": "1.15.0",
+                "required_version": ">=1.7.0"
+            }
+        ]
+    },
+    {
+        "package": {
+            "key": "requests",
+            "package_name": "requests",
+            "installed_version": "2.23.0"
+        },
+        "dependencies": [
+            {
+                "key": "idna",
+                "package_name": "idna",
+                "installed_version": "2.9",
+                "required_version": ">=2.5,<3"
+            },
+            {
+                "key": "chardet",
+                "package_name": "chardet",
+                "installed_version": "3.0.4",
+                "required_version": ">=3.0.2,<4"
+            },
+            {
+                "key": "certifi",
+                "package_name": "certifi",
+                "installed_version": "2020.4.5.2",
+                "required_version": ">=2017.4.17"
+            },
+            {
+                "key": "urllib3",
+                "package_name": "urllib3",
+                "installed_version": "1.25.9",
+                "required_version": ">=1.21.1,<1.26,!=1.25.1,!=1.25.0"
+            }
+        ]
+    },
+    {
+        "package": {
+            "key": "regex",
+            "package_name": "regex",
+            "installed_version": "2020.9.27"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "pyyaml",
+            "package_name": "PyYAML",
+            "installed_version": "5.3.1"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "pywin32-ctypes",
+            "package_name": "pywin32-ctypes",
+            "installed_version": "0.2.0"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "pytz",
+            "package_name": "pytz",
+            "installed_version": "2020.1"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "python-dateutil",
+            "package_name": "python-dateutil",
+            "installed_version": "2.8.1"
+        },
+        "dependencies": [
+            {
+                "key": "six",
+                "package_name": "six",
+                "installed_version": "1.15.0",
+                "required_version": ">=1.5"
+            }
+        ]
+    },
+    {
+        "package": {
+            "key": "pyqt5",
+            "package_name": "PyQt5",
+            "installed_version": "5.11.3"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "pyproj",
+            "package_name": "pyproj",
+            "installed_version": "2.6.1.post1"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "pyinstaller",
+            "package_name": "pyinstaller",
+            "installed_version": "4.0"
+        },
+        "dependencies": [
+            {
+                "key": "setuptools",
+                "package_name": "setuptools",
+                "installed_version": "50.3.0",
+                "required_version": null
+            },
+            {
+                "key": "pywin32-ctypes",
+                "package_name": "pywin32-ctypes",
+                "installed_version": "0.2.0",
+                "required_version": ">=0.2.0"
+            },
+            {
+                "key": "altgraph",
+                "package_name": "altgraph",
+                "installed_version": "0.17",
+                "required_version": null
+            },
+            {
+                "key": "pyinstaller-hooks-contrib",
+                "package_name": "pyinstaller-hooks-contrib",
+                "installed_version": "2020.8",
+                "required_version": ">=2020.6"
+            },
+            {
+                "key": "pefile",
+                "package_name": "pefile",
+                "installed_version": "2019.4.18",
+                "required_version": ">=2017.8.1"
+            }
+        ]
+    },
+    {
+        "package": {
+            "key": "pyinstaller-hooks-contrib",
+            "package_name": "pyinstaller-hooks-contrib",
+            "installed_version": "2020.8"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "pygments",
+            "package_name": "Pygments",
+            "installed_version": "2.5.2"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "pydeps",
+            "package_name": "pydeps",
+            "installed_version": "1.9.13"
+        },
+        "dependencies": [
+            {
+                "key": "stdlib-list",
+                "package_name": "stdlib-list",
+                "installed_version": "0.8.0",
+                "required_version": null
+            }
+        ]
+    },
+    {
+        "package": {
+            "key": "psycopg2-binary",
+            "package_name": "psycopg2-binary",
+            "installed_version": "2.8.4"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "plotly",
+            "package_name": "plotly",
+            "installed_version": "4.8.2"
+        },
+        "dependencies": [
+            {
+                "key": "six",
+                "package_name": "six",
+                "installed_version": "1.15.0",
+                "required_version": null
+            },
+            {
+                "key": "retrying",
+                "package_name": "retrying",
+                "installed_version": "1.3.3",
+                "required_version": ">=1.3.3"
+            }
+        ]
+    },
+    {
+        "package": {
+            "key": "pipdeptree",
+            "package_name": "pipdeptree",
+            "installed_version": "2.0.0"
+        },
+        "dependencies": [
+            {
+                "key": "pip",
+                "package_name": "pip",
+                "installed_version": "20.2.2",
+                "required_version": ">=6.0.0"
+            }
+        ]
+    },
+    {
+        "package": {
+            "key": "pip",
+            "package_name": "pip",
+            "installed_version": "20.2.2"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "pefile",
+            "package_name": "pefile",
+            "installed_version": "2019.4.18"
+        },
+        "dependencies": [
+            {
+                "key": "future",
+                "package_name": "future",
+                "installed_version": "0.16.0",
+                "required_version": null
+            }
+        ]
+    },
+    {
+        "package": {
+            "key": "pbr",
+            "package_name": "pbr",
+            "installed_version": "4.3.0"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "pathspec",
+            "package_name": "pathspec",
+            "installed_version": "0.8.0"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "owslib",
+            "package_name": "OWSLib",
+            "installed_version": "0.20.0"
+        },
+        "dependencies": [
+            {
+                "key": "requests",
+                "package_name": "requests",
+                "installed_version": "2.23.0",
+                "required_version": ">=1.0"
+            },
+            {
+                "key": "pyproj",
+                "package_name": "pyproj",
+                "installed_version": "2.6.1.post1",
+                "required_version": ">=2"
+            },
+            {
+                "key": "pytz",
+                "package_name": "pytz",
+                "installed_version": "2020.1",
+                "required_version": null
+            },
+            {
+                "key": "pyyaml",
+                "package_name": "pyyaml",
+                "installed_version": "5.3.1",
+                "required_version": null
+            },
+            {
+                "key": "python-dateutil",
+                "package_name": "python-dateutil",
+                "installed_version": "2.8.1",
+                "required_version": ">=1.5"
+            }
+        ]
+    },
+    {
+        "package": {
+            "key": "numpy",
+            "package_name": "numpy",
+            "installed_version": "1.19.1"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "nose2",
+            "package_name": "nose2",
+            "installed_version": "0.8.0"
+        },
+        "dependencies": [
+            {
+                "key": "six",
+                "package_name": "six",
+                "installed_version": "1.15.0",
+                "required_version": ">=1.7"
+            },
+            {
+                "key": "coverage",
+                "package_name": "coverage",
+                "installed_version": "4.5.1",
+                "required_version": ">=4.4.1"
+            }
+        ]
+    },
+    {
+        "package": {
+            "key": "nbformat",
+            "package_name": "nbformat",
+            "installed_version": "4.4.0"
+        },
+        "dependencies": [
+            {
+                "key": "jupyter-core",
+                "package_name": "jupyter-core",
+                "installed_version": "4.4.0",
+                "required_version": null
+            },
+            {
+                "key": "traitlets",
+                "package_name": "traitlets",
+                "installed_version": "4.3.3",
+                "required_version": ">=4.1"
+            },
+            {
+                "key": "ipython-genutils",
+                "package_name": "ipython-genutils",
+                "installed_version": "0.2.0",
+                "required_version": null
+            },
+            {
+                "key": "jsonschema",
+                "package_name": "jsonschema",
+                "installed_version": "2.6.0",
+                "required_version": ">=2.4,!=2.5.0"
+            }
+        ]
+    },
+    {
+        "package": {
+            "key": "mypy-extensions",
+            "package_name": "mypy-extensions",
+            "installed_version": "0.4.3"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "mock",
+            "package_name": "mock",
+            "installed_version": "2.0.0"
+        },
+        "dependencies": [
+            {
+                "key": "pbr",
+                "package_name": "pbr",
+                "installed_version": "4.3.0",
+                "required_version": ">=0.11"
+            },
+            {
+                "key": "six",
+                "package_name": "six",
+                "installed_version": "1.15.0",
+                "required_version": ">=1.9"
+            }
+        ]
+    },
+    {
+        "package": {
+            "key": "markupsafe",
+            "package_name": "MarkupSafe",
+            "installed_version": "1.1.1"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "jupyter-core",
+            "package_name": "jupyter-core",
+            "installed_version": "4.4.0"
+        },
+        "dependencies": [
+            {
+                "key": "traitlets",
+                "package_name": "traitlets",
+                "installed_version": "4.3.3",
+                "required_version": null
+            }
+        ]
+    },
+    {
+        "package": {
+            "key": "jsonschema",
+            "package_name": "jsonschema",
+            "installed_version": "2.6.0"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "jinja2",
+            "package_name": "Jinja2",
+            "installed_version": "2.11.1"
+        },
+        "dependencies": [
+            {
+                "key": "markupsafe",
+                "package_name": "MarkupSafe",
+                "installed_version": "1.1.1",
+                "required_version": ">=0.23"
+            }
+        ]
+    },
+    {
+        "package": {
+            "key": "isort",
+            "package_name": "isort",
+            "installed_version": "5.5.3"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "ipython-genutils",
+            "package_name": "ipython-genutils",
+            "installed_version": "0.2.0"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "idna",
+            "package_name": "idna",
+            "installed_version": "2.9"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "httplib2",
+            "package_name": "httplib2",
+            "installed_version": "0.11.3"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "gdal",
+            "package_name": "GDAL",
+            "installed_version": "3.1.4"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "future",
+            "package_name": "future",
+            "installed_version": "0.16.0"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "decorator",
+            "package_name": "decorator",
+            "installed_version": "4.4.1"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "coverage",
+            "package_name": "coverage",
+            "installed_version": "4.5.1"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "click",
+            "package_name": "click",
+            "installed_version": "7.1.2"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "chardet",
+            "package_name": "chardet",
+            "installed_version": "3.0.4"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "certifi",
+            "package_name": "certifi",
+            "installed_version": "2020.4.5.2"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "black",
+            "package_name": "black",
+            "installed_version": "20.8b1"
+        },
+        "dependencies": [
+            {
+                "key": "click",
+                "package_name": "click",
+                "installed_version": "7.1.2",
+                "required_version": ">=7.1.2"
+            },
+            {
+                "key": "typed-ast",
+                "package_name": "typed-ast",
+                "installed_version": "1.4.1",
+                "required_version": ">=1.4.0"
+            },
+            {
+                "key": "toml",
+                "package_name": "toml",
+                "installed_version": "0.10.1",
+                "required_version": ">=0.10.1"
+            },
+            {
+                "key": "mypy-extensions",
+                "package_name": "mypy-extensions",
+                "installed_version": "0.4.3",
+                "required_version": ">=0.4.3"
+            },
+            {
+                "key": "regex",
+                "package_name": "regex",
+                "installed_version": "2020.9.27",
+                "required_version": ">=2020.1.8"
+            },
+            {
+                "key": "pathspec",
+                "package_name": "pathspec",
+                "installed_version": "0.8.0",
+                "required_version": ">=0.6,<1"
+            },
+            {
+                "key": "appdirs",
+                "package_name": "appdirs",
+                "installed_version": "1.4.4",
+                "required_version": null
+            },
+            {
+                "key": "typing-extensions",
+                "package_name": "typing-extensions",
+                "installed_version": "3.7.4.3",
+                "required_version": ">=3.7.4"
+            }
+        ]
+    },
+    {
+        "package": {
+            "key": "appdirs",
+            "package_name": "appdirs",
+            "installed_version": "1.4.4"
+        },
+        "dependencies": []
+    },
+    {
+        "package": {
+            "key": "altgraph",
+            "package_name": "altgraph",
+            "installed_version": "0.17"
+        },
+        "dependencies": []
+    }
+]
